46704a6468f6c0fa2558017a007898c0030516ee
Abhijit Mishra <pallabinayak11@gmail.com>
2024-04-03 12:09:14 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,36 @@
+/**
+ *
+ * <div id="parent">
+ *      <div id="child">
+ *         <h1>I'm h1 tag</h1>
+ *         <h2>I'm h1 tag</h2>
+ *      </div>
+ * <div id="child2">
+ *         <h1>I'm h1 tag</h1>
+ *         <h2>I'm h1 tag</h2>
+ *      </div>
+ * </div>
+ *
+ * ReactElement(Object) => HTML(Browser Understands)
+ */
+
+
+const parent = React.createElement("div", { id: "parent" },
+    [
+        React.createElement("div", { id: "child" }, [
+            React.createElement("h1", {}, "I am H1 Tag"),
+            React.createElement("h2", {}, "I am H2 Tag"),
+        ]),
+        React.createElement("div", { id: "child2" }, [
+            React.createElement("h1", {}, "I am H1 Tag"),
+            React.createElement("h2", {}, "I am H2 Tag"),
+        ])
+    ]
+)
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
