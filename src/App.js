3899ab7a660ecb71813c97d32157e59f5d918c2d
Abhijit Mishra <pallabinayak11@gmail.com>
2024-04-04 23:15:56 +0530

    episode-7(children-route)

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Outlet,RouterProvider, createBrowserRouter } from "react-router-dom";
 import About from "./components/About";
 import Contact from "./components/Contact";
 
@@ -10,23 +10,29 @@ const AppLayout = () => {
   return (
     <div className="app">
       <Header />
-      <Body />
+      <Outlet />
     </div>
   );
 };
 const appRoute = createBrowserRouter([
   {
     path: "/",
-    element: <AppLayout />,
-  },
-  {
-    path: "/about",
-    element: <About />,
-  },
-  {
-    path: "/contact",
-    element: <Contact />,
-  },
+    element:<AppLayout/>,
+        children:[
+            {
+                path:"/",
+                element:<Body/>
+            },
+            {
+                path:"/about",
+                element:<About/>
+            },
+            {
+                path:"/contact",
+                element:<Contact/>
+            }
+        ]
+    }
 ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<RouterProvider router={appRoute} />);
--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ const About = () => {
     return (
         <div>
             <h1>About Me</h1>
-            <h2>WELCOME TO ABHIJIT FOOD CLUB</h2>
+            <h2>WELCOME TO Pallabi FOOD CLUB</h2>
         </div>
     )
 }
