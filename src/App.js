f8ce5e611be6fd82e18c258de3ba1462e39c2a96
Abhijit Mishra <pallabinayak11@gmail.com>
2024-04-04 23:12:36 +0530

    episode-7-(routing)

--- src/App.js
+++ src/App.js
@@ -2,15 +2,31 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
 const AppLayout = () => {
-    return (
-        <div className="app">
-            <Header />
-            <Body />
-        </div>
-    )
-}
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
+const appRoute = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRoute} />);
---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+    return (
+        <div>
+            <h1>About Me</h1>
+            <h2>WELCOME TO ABHIJIT FOOD CLUB</h2>
+        </div>
+    )
+}
+
+export default About;
