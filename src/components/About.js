ff950c6888e6088d635724cf784b04dfff02c85f
Abhijit Mishra <pallabinayak11@gmail.com>
2024-04-05 18:57:53 +0530

    how to change state variable in class based component

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,4 @@
-import User from "./User";
+
 import UserClass from "./User-Class";
 
 const About = () => {
@@ -6,7 +6,7 @@ const About = () => {
         <div>
             <h1>About Me</h1>
             <h2>WELCOME TO Pallabi FOOD CLUB</h2>
-            <User name={"Pallabi  (Function)"} location={"Nuapada(Function)"}/>
+        
             <UserClass name={"Pallabi(Class)"} location={"Nuapada(Class)"} />
         </div>
     )
--- src/components/User-Class.js
+++ src/components/User-Class.js
@@ -18,6 +18,13 @@ class UserClass extends React.Component{
             <div className='user-card'>
                   <h2>Count:{this.state.count}</h2>
                <h2>Count2:{this.state.count2}</h2>
+               <button
+          onClick={() => {
+            this.setState({ count: this.state.count + 1 });
+          }}
+        >
+          Count
+        </button>
                <h2>Name: {name}</h2>
                <h3>Location:{location}</h3>    
             </div>
